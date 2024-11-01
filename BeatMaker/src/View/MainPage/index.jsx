5eeaced68d7104cee1ed39c2d7daf7a20af76fa1
Eduardo Manzur Finotti Seabra <eduardo.manzur@dinfor.uniceub>
2024-11-01 10:18:25 -0300

    Update 01

--- BeatMaker/src/View/MainPage/index.jsx
+++ BeatMaker/src/View/MainPage/index.jsx
@@ -1,11 +1,15 @@
 import "./style.css";
 import Header from "../../Componentes/Header";
 import Footer from "../../Componentes/Footer";
+import BeatMaker from "../../Componentes/BeatMaker";
+import Piano from "../../Componentes/Piano";
 
 export default function MainPage() {
     return (
         <>
             <Header/>
+            <BeatMaker/>
+            <Piano/>
             <Footer/>
         </>
     )
