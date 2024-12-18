152aa39cb4e5374f4b5cad41d4cc48f740dcd256
Guiffans <guirbarros.dev@gmail.com>
2024-12-18 11:42:42 -0300

    seilamano

--- BeatMaker/vite.config.js
+++ BeatMaker/vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === 'production' ? '/React_BeatMaker/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+  base:"/",
+
 })
